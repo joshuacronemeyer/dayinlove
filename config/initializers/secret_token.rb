6310a03388dcd733a14efd1e0241941c453718aa
Josh Cronemeyer <joshuacronemeyer+github@gmail.com>
2013-02-23 14:36:49 -0800

    initial checkin of new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dayinlove::Application.config.secret_token = '6e8678e9eac867e5bd68e8352b6adef4c8c5ee6c1fc17d9c7d3207f0a367cbfd6a3c153e860ca15d9de3484dc3c9aa052ae97da724dcccbff4f8966fd47a9f1c'
