bf7bfb7700fca8784f4a7f60f039dd294ac71368
Josh Cronemeyer <joshuacronemeyer+github@gmail.com>
2013-02-28 22:11:18 -0800

    hmm.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,10 +3,11 @@ class PagesController < ApplicationController
   end
 
   def auth
-    auth = request.env["omniauth.auth"]
+    # auth = request.env["omniauth.auth"]
     # user = User.find_by_uid(auth["uid"]) || User.create_with_omniauth(auth)
     # session[:user_id] = user.id
-    p auth
+    # p auth
+    p "HERE"
     redirect_to root_url, :notice => "Signed in!"
   end
 end
