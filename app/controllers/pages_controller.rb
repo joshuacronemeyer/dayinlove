137da81e8d54975f2ab68b003f9bfe61311e033f
Josh Cronemeyer <joshuacronemeyer+github@gmail.com>
2013-02-28 21:55:23 -0800

    action for coming back from oauth land

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def auth
+    auth = request.env["omniauth.auth"]
+    # user = User.find_by_uid(auth["uid"]) || User.create_with_omniauth(auth)
+    # session[:user_id] = user.id
+    p auth
+    redirect_to root_url, :notice => "Signed in!"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Dayinlove::Application.routes.draw do
   match 'pages/home' => 'pages#home', :as => :home
+  match 'auth' => 'pages#auth'
   root :to => 'pages#home'
 end
