1ee97ffb4d4c7a19085f7a3657f14da2383750a8
Josh Cronemeyer <joshuacronemeyer+github@gmail.com>
2013-02-28 22:13:58 -0800

    docs are good

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Dayinlove::Application.routes.draw do
   match 'pages/home' => 'pages#home', :as => :home
   match 'auth' => 'pages#auth'
+  match '/auth/twitter/callback', to: 'pages#auth'
   root :to => 'pages#home'
 end
