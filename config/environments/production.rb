77e8e7a3d94e002c6e5bc48859a8cfcb0a0f0878
Josh Cronemeyer <joshuacronemeyer+github@gmail.com>
2013-02-28 16:08:05 -0800

    try it out in the real world

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Dayinlove::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-
+  config.middleware.use Rack::Today
+  
   # Code is not reloaded between requests
   config.cache_classes = true
 
