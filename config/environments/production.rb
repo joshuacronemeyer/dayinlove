66ed576484549f8bfa31775c9ac636d4815e957d
Josh Cronemeyer <joshuacronemeyer+github@gmail.com>
2013-03-03 22:24:31 -0800

    still not able to precomplie assets on hoerku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,5 @@
+require "#{Rails.root}/middleware/rack/today"
+
 Dayinlove::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
   config.middleware.use Rack::Today
