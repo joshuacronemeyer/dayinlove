6aff9bd3a8f9de57829d7008bab21211fa08a07f
Josh Cronemeyer <joshuacronemeyer@gmail.com>
2013-03-20 00:00:28 -0700

    make the text vary by day

--- lib/love_request.rb
+++ lib/love_request.rb
@@ -7,8 +7,23 @@ class LoveRequest
     if @mention.media && !@mention.media.empty?
       photo = @mention.media.first
       url = photo.media_url
-      CompositeImage.new(url, ["Friday", "I'm in love"]).delay.composite!
+      CompositeImage.new(url, [today, messages[today]]).delay.composite!
     end
+  end
+
+  private
+  def today
+    Date.today.strftime('%A')
+  end
+
+  def messages
+    {"Monday" => "You can fall apart",
+    "Tuesday" => "Break my heart",
+    "Wednesday" => "Break my heart",
+    "Thursday" => "Doesn't even start",
+    "Friday" => "I'm in love",
+    "Saturday" => "Wait...",
+    "Sunday" => "Always comes too late"}
   end
 
 end
